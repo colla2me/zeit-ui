ac79713b804e38f77ddeafcbba3c1fc7e8a65f7e
samuel <zhangleven@gmail.com>
2020-03-07 19:22:08 +0800

    setup project

---
+++ example.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import { readSync } from 'fs';
+
+ReactDOM.render(
+  <div>React-zeit-ui</div>,
+document.querySelector('#root'));
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const {CheckerPlugin} = require('awesome-typescript-loader')
+
+const devMode = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  entry: {
+    index: './lib/index.tsx'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'zeit-ui',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    alias: {
+      stylesheets: path.resolve(__dirname, 'stylesheets'),
+      examples: path.resolve(__dirname, 'examples'),
+      lib: path.resolve(__dirname, 'lib'),
+    },
+    modules: [path.resolve(__dirname, 'include'), 'node_modules']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {test: /\.tsx?$/, loader: 'awesome-typescript-loader'},
+      {
+        test: /\.tsx?$/,
+        enforce: 'pre',
+        use: [{loader: 'tslint-loader'}],
+      },
+      {
+        test: /icons.+\.svg$/,
+        loader: 'svg-sprite-loader',
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.md$/,
+        loader: 'text-loader',
+      },
+      {
+        test: /\.s([ac])ss$/,
+        use: [
+          devMode ? 'style-loader' : {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // publicPath: '../'
+            }
+          },
+          'css-loader',
+          {
+            loader: "sass-loader",
+            options: {
+              includePaths: [path.resolve(__dirname, 'stylesheets', 'include')]
+            }
+          }]
+      }
+    ],
+  },
+  plugins: [
+    new CheckerPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    })
+  ],
+  resolveLoader: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'loaders')
+    ]
+  }
+}
